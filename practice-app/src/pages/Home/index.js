89b4fd365aa2f929e1dbb64706d85c8cdc2b77f0
Eric Hay <ericdanielhay@yahoo.com>
2022-08-15 12:52:44 -0500

    Updated search results page to display results and updated css.

--- practice-app/src/pages/Home/index.js
+++ practice-app/src/pages/Home/index.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
 import "./style.css";
 
 library.add(faMagnifyingGlass);
 
 function Home() {
+  const navigate = useNavigate();
   const [search, setSearch] = useState([]);
 
   const handleInputChange = (event) => {
@@ -17,22 +20,24 @@ function Home() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(search);
-    const api_url =
-      "https://public.gis.lacounty.gov/public/rest/services/LACounty_Dynamic/LMS_Data_Public/MapServer/158/query?where=ZIP='" +
+    axios
+      .get("https://public.gis.lacounty.gov/public/rest/services/LACounty_Dynamic/LMS_Data_Public/MapServer/158/query?where=ZIP='" +
       search +
-      "'&outFields=*&returnGeometry=false&outSR=4326&f=json";
-
-    async function getapi(url) {
-      const response = await fetch(url);
-      let shelters = await response.json();
-      console.log(shelters.features);
-    }
-
-    getapi(api_url);
-  };
+      "'or CITY='" +
+      search +
+      "'&outFields=*&returnGeometry=false&outSR=4326&f=json")
+      .then((response) => {
+        console.log(response.data);
+        let results = response.data
+        let test = results.features;
+        console.log(test)
+        navigate("/results",{state:{response:results}})
+      })
+      .catch((err) => console.log(err));
+    };
 
   return (
+    <div className="homeBackground">
     <div className="home container-fluid">
       <h2 className="homeFont">Let's find a place for you to stay!</h2>
       <form className="d-flex" role="search">
@@ -51,6 +56,7 @@ function Home() {
           <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
         </button>
       </form>
+    </div>
     </div>
   );
 }
--- practice-app/src/pages/SearchResults/index.js
+++ practice-app/src/pages/SearchResults/index.js
@@ -1,8 +1,74 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faClock } from "@fortawesome/free-regular-svg-icons";
+import { faCity, faPhone, faLink } from "@fortawesome/free-solid-svg-icons";
 import "./style.css";
 
-function SearchResults() {
+library.add(faClock, faCity, faPhone, faLink);
 
-  return <h1>This is the Search page...</h1>;
+function SearchResults() {
+  const location = useLocation();
+  const data = location.state.response;
+  console.log(
+    "This is the list from the results page: " + JSON.stringify(data.features)
+  );
+
+  return (
+    <div className="shelter-container container-fluid">
+      {data.features !== undefined && data.features !== null ? (
+        <ul className="shelter-list list-group">
+          {data.features.map((shelter) => (
+            <li className="shelter-item list-group-item" key={shelter.id}>
+              {shelter.attributes.Name !== undefined && shelter.attributes.Name !== null ? (
+              <div>
+                <h3>{shelter.attributes.Name}</h3>
+              </div>) : (
+        <></>
+      )}
+      {shelter.attributes.hours !== undefined && shelter.attributes.hours !== null ? (
+              <div>
+                <FontAwesomeIcon icon="fa-regular fa-clock" />{" "}
+                {shelter.attributes.hours}
+              </div>) : (
+        <></>
+      )}
+      {shelter.attributes.addrln1 !== undefined && shelter.attributes.addrln1 !== null ? (
+              <div>
+                <FontAwesomeIcon icon="fa-solid fa-city" />{" "}
+                {shelter.attributes.addrln1}, {shelter.attributes.city}
+              </div>) : (
+        <></>
+      )}
+      {shelter.attributes.phones !== undefined && shelter.attributes.phones !== null ? (
+              <div>
+                <FontAwesomeIcon icon="fa-solid fa-phone" />{" "}
+                {shelter.attributes.phones}
+              </div>) : (
+        <></>
+      )}
+      {shelter.attributes.url !== undefined && shelter.attributes.url !== null && shelter.attributes.url !== "NULL" ? (
+              <div>
+                <FontAwesomeIcon icon="fa-solid fa-link" />{" "}
+                <strong><a href={"https://" + shelter.attributes.url}>{shelter.attributes.url}</a></strong>
+              </div>) : (
+        <></>
+      )}
+      {shelter.attributes.description !== undefined && shelter.attributes.description !== null ? (
+              <div className="shelterDescription">
+                <em>{shelter.attributes.description}</em>
+              </div>) : (
+        <></>
+      )}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <h3>No Results To Display</h3>
+      )}
+    </div>
+  );
 }
 
 export default SearchResults;
